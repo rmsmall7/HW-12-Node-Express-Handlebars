55203b431af71448e537536afbd2ec10b93c4212
unknown <rmsmall7@gmail.com>
2019-05-17 20:07:19 -0400

    updated connection.js for jawsdb

--- config/connection.js
+++ config/connection.js
@@ -4,15 +4,21 @@
 var mysql = require('mysql');
 // var config = require('dotenv')
 require('dotenv').config();
+var connection; 
+
 
 // connection to the DB
-var connection = mysql.createConnection({
+if(process.env.JAWSDB_URL){
+  connection=mysql.createConnection(process.env.JAWSDB_URL);
+}else{  
+connection = mysql.createConnection({
   host: process.env.DB_HOST,
   port: 3306,
   user: process.env.DB_USER,
   password: process.env.DB_PASS,
   database: 'burgers_db'
-})
+});
+};
 
 // testing the connection
 connection.connect(function(err){
